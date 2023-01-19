025f022dc23d127d9b3f20f0be53a7929777ebe5
Shivam Hajare <shivamhajare96@gmail.com>
2023-01-19 19:58:16 +0530

    checking2.0

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const dotenv = require("dotenv")
 var cors = require('cors')
+app.use(cors({
+    origin: 'https://thriving-rolypoly-cb9f6b.netlify.app',
+    credentials: true
+}));
 const express = require("express")
 const app = express();
 
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
-app.use(cors())
+// 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://thriving-rolypoly-cb9f6b.netlify.app");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
